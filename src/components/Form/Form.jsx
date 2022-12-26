831ad325ed32c4b7ecda69f5027be290834bd884
Matheus Uchoa <muchoa@motorola.com>
2022-12-26 15:16:14 -0400

    teste

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,6 +14,7 @@ const Form = ({ onSubmit }) => {
         <input type="email" placeholder="Insira seu e-mail..." />
         <button type="submit">Seguir</button>
     </form>
+    //teste de commmit 
 }
 
 export default Form
